1114cb92c0aeedddb7c9a0d888602f57908c378d
lorenzo.s <spataro@fondazionecni.it>
2024-09-03 11:26:44 +0200

    Allow all hosts for connection

--- barber_shop/settings.py
+++ barber_shop/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-3hnsremjeim!43ez0m*3a)rdwukwjen#i!g9pmlroj4t3t9so7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
