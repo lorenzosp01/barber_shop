dcac49ed0fe511bbcd0e9ad4f0b0a4b60261dc70
lorenzo.s <spataro@fondazionecni.it>
2024-09-04 14:24:57 +0200

    Add benchmarks

---
+++ api/ec2-benchmarking/lib/http.go
@@ -0,0 +1,283 @@
+package lib
+
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"mime/multipart"
+	"net/http"
+	"os"
+	"sync"
+	"time"
+)
+
+type Payload struct {
+	username string
+	password string
+	title    string
+	content  string
+	rating   int
+}
+
+const defaultEndpoint = "http://localhost:8000"
+
+const defaultTimeout = 1 * time.Second
+
+func timeHTTPRequest(url string, withBody bool, token string) (string, int, error) {
+	// Add the default endpoint if the URL is relative
+	if url[0] == '/' {
+		url = defaultEndpoint + url
+	}
+
+	client := http.Client{
+		Timeout: defaultTimeout,
+	}
+
+	start := time.Now()
+
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return "", 0, err
+	}
+
+	if token != "" {
+		req.Header.Add("Authorization", "Bearer "+token)
+	}
+	resp, err := client.Do(req)
+	if err != nil {
+		// T = Timeout
+		fmt.Fprintf(os.Stderr, "\033[1;31mT\033[0m")
+		os.Stderr.Sync()
+		return "", 0, err
+	}
+	defer resp.Body.Close()
+
+	// Check that status code is 200
+	if resp.StatusCode != http.StatusOK {
+		// F = Failed request
+		fmt.Fprintf(os.Stderr, "\033[1;31mF\033[0m")
+		os.Stderr.Sync()
+		return "", 0, fmt.Errorf("status code %d", resp.StatusCode)
+	}
+
+	// Calculate the time taken for the request in milliseconds
+	duration := time.Since(start).Milliseconds()
+
+	// Get the response body as a string, if needed
+	var body string
+	if withBody {
+		bodyBytes, err := io.ReadAll(resp.Body)
+		if err != nil {
+			return "", 0, err
+		}
+		body = string(bodyBytes)
+	} else {
+		// Discard the response body, if not needed
+		body = ""
+	}
+
+	// Print the success, as a green dot
+	fmt.Fprintf(os.Stderr, "\033[32m.\033[0m")
+	os.Stderr.Sync()
+
+	return body, int(duration), nil
+}
+
+func timeHTTPPost(url string, data Payload, token string) (string, int, error) {
+	// Add the default endpoint if the URL is relative
+	if url[0] == '/' {
+		url = defaultEndpoint + url
+	}
+
+	client := http.Client{
+		Timeout: defaultTimeout,
+	}
+
+	var requestBody bytes.Buffer
+	writer := multipart.NewWriter(&requestBody)
+
+	err := writer.WriteField("username", data.username)
+	if err != nil {
+		return "", 0, err
+	}
+
+	err = writer.WriteField("password", data.password)
+	if err != nil {
+		return "", 0, err
+	}
+
+	if err := writer.Close(); err != nil {
+		log("Error: could not close writer")
+		panic(err)
+	}
+
+	// Create the request (without sending it)
+	req, err := http.NewRequest("POST", url, &requestBody)
+	if err != nil {
+		log("Error: could not create request")
+		panic(err)
+	}
+
+	// Set the content type
+	req.Header.Set("Content-Type", writer.FormDataContentType())
+
+	if token != "" {
+		// Set the 	authorization header
+		req.Header.Set("Authorization", "Bearer "+token)
+	}
+
+	// Send the request
+	start := time.Now()
+	resp, err := client.Do(req)
+	if err != nil {
+		// T = Timeout
+		fmt.Fprintf(os.Stderr, "\033[1;31mT\033[0m")
+		os.Stderr.Sync()
+		return "", 0, err
+	}
+	defer resp.Body.Close()
+
+	// Check that status code is 200
+	if resp.StatusCode != http.StatusOK {
+		// F = Failed request
+		fmt.Fprintf(os.Stderr, "\033[1;31mF\033[0m")
+		os.Stderr.Sync()
+		return "", 0, fmt.Errorf("status code %d", resp.StatusCode)
+	}
+
+	// Calculate the time taken for the request in milliseconds
+	duration := time.Since(start).Milliseconds()
+
+	// Get the response body as a string
+	bodyBytes, err := io.ReadAll(resp.Body)
+	if err != nil {
+		// F = Failed request
+		fmt.Fprintf(os.Stderr, "\033[1;31mF\033[0m")
+		os.Stderr.Sync()
+		return "", 0, err
+	}
+	body := string(bodyBytes)
+
+	// Print the success, as a green dot
+	fmt.Fprintf(os.Stderr, "\033[32m.\033[0m")
+	os.Stderr.Sync()
+
+	return body, int(duration), nil
+}
+
+func timeHTTPPostFile(url string, data Payload, filePath string, fileName string, token string) (string, int, error) {
+	// Add the default endpoint if the URL is relative
+	if url[0] == '/' {
+		url = defaultEndpoint + url
+	}
+
+	client := http.Client{
+		Timeout: defaultTimeout,
+	}
+
+	file, err := os.Open(filePath)
+	if err != nil {
+		log("Error: could not open file", filePath)
+		panic(err)
+	}
+	defer file.Close()
+
+	var requestBody bytes.Buffer
+	writer := multipart.NewWriter(&requestBody)
+
+	// Add the file to the request
+	part, err := writer.CreateFormFile("image", fileName)
+	if err != nil {
+		log("Error: could not create form file")
+		panic(err)
+	}
+
+	if _, err := io.Copy(part, file); err != nil {
+		log("Error: could not copy file to part")
+		panic(err)
+	}
+
+	err = writer.WriteField("title", data.title)
+	if err != nil {
+		return "", 0, err
+	}
+
+	err = writer.WriteField("content", data.content)
+	if err != nil {
+		return "", 0, err
+	}
+	err = writer.WriteField("rating", fmt.Sprintf("%d", data.rating))
+	if err != nil {
+		return "", 0, err
+	}
+
+	if err := writer.Close(); err != nil {
+		log("Error: could not close writer")
+		panic(err)
+	}
+
+	// Create the request (without sending it)
+	req, err := http.NewRequest("POST", url, &requestBody)
+	if err != nil {
+		log("Error: could not create request")
+		panic(err)
+	}
+
+	// Set the content type
+	req.Header.Set("Content-Type", writer.FormDataContentType())
+
+	// Set the authorization header
+	req.Header.Set("Authorization", "Bearer "+token)
+
+	// Send the request
+	start := time.Now()
+	resp, err := client.Do(req)
+	if err != nil {
+		// T = Timeout
+		fmt.Fprintf(os.Stderr, "\033[1;31mT\033[0m")
+		os.Stderr.Sync()
+		return "", 0, err
+	}
+	defer resp.Body.Close()
+
+	// Check that status code is 200
+	if resp.StatusCode != http.StatusOK {
+		// F = Failed request
+		fmt.Fprintf(os.Stderr, "\033[1;31mF\033[0m")
+		os.Stderr.Sync()
+		return "", 0, fmt.Errorf("status code %d", resp.StatusCode)
+	}
+
+	// Calculate the time taken for the request in milliseconds
+	duration := time.Since(start).Milliseconds()
+
+	// Get the response body as a string
+	bodyBytes, err := io.ReadAll(resp.Body)
+	if err != nil {
+		// F = Failed request
+		fmt.Fprintf(os.Stderr, "\033[1;31mF\033[0m")
+		os.Stderr.Sync()
+		return "", 0, err
+	}
+	body := string(bodyBytes)
+
+	// Print the success, as a green dot
+	fmt.Fprintf(os.Stderr, "\033[32m.\033[0m")
+	os.Stderr.Sync()
+
+	return body, int(duration), nil
+}
+
+func TimeHTTPRequest(url string) (int, error) {
+	_, duration, err := timeHTTPRequest(url, false, "")
+	return duration, err
+}
+
+func TimeHTTPRequestWithBody(url string) (string, int, error) {
+	return timeHTTPRequest(url, true, "")
+}
+
+func TimeHTTPRequestWaiting(url string, wg *sync.WaitGroup) (int, error) {
+	defer wg.Done()
+	return TimeHTTPRequest(url)
+}
---
+++ api/ec2-benchmarking/lib/user-simulator.go
@@ -0,0 +1,271 @@
+package lib
+
+import (
+	"encoding/json"
+	"fmt"
+	"math"
+	"math/rand"
+	"os"
+	"sync"
+	"time"
+)
+
+type UserSimulation struct {
+	username       string
+	password       string
+	currentState   string
+	token          string
+	durationsMutex sync.Mutex
+	durations      []int
+	failedRequests int
+}
+
+type UserSimulationStats struct {
+	ValidRequests  int
+	FailedRequests int
+	TimeMean       float64
+	TimeStdDev     float64
+}
+
+func NewUserSimulation(username string, password string) *UserSimulation {
+
+	return &UserSimulation{
+		currentState:   startState,
+		username:       username,
+		password:       password,
+		durationsMutex: sync.Mutex{},
+		durations:      []int{},
+		failedRequests: 0,
+	}
+}
+
+const debugLog = false
+
+func log(args ...any) {
+	if debugLog {
+		fmt.Println(args...)
+	}
+}
+
+const startState = "start"
+const uploadReviewState = "uploadReview"
+const listPersonalReviewsState = "listPersonalReviews"
+const listAllReviewsState = "listAllReviews"
+
+func (u *UserSimulation) login() error {
+	log("Logging in...")
+
+	payload := Payload{
+		username: u.username,
+		password: u.password,
+	}
+	body, duration, err := timeHTTPPost("/auth/get-token", payload, "")
+	if err != nil {
+		return err
+	}
+
+	u.collectDuration(duration)
+	// Parse the response to get the token
+	// The response is a JSON object with a key "token"
+	var data map[string]interface{}
+	if err := json.Unmarshal([]byte(body), &data); err != nil {
+		log("Error: login JSON is not valid")
+		return err
+	}
+
+	u.token = data["access_token"].(string)
+
+	return nil
+
+}
+func (u *UserSimulation) start() string {
+	log("Entering start state...")
+
+	if u.token == "" {
+		err := u.login()
+		if err != nil {
+			log("Error: login failed")
+			return startState
+		}
+	}
+	next := rand.Float32()
+	if next < 0.1 {
+		// 10%
+		return uploadReviewState
+	}
+	if next < 0.3 {
+		// 20%
+		return listPersonalReviewsState
+	}
+	// 70%
+	return listAllReviewsState
+}
+
+func (u *UserSimulation) listPersonalReviews() string {
+	log("Entering listReviews state...")
+
+	_, duration, err := timeHTTPRequest("/api/list-user-reviews", true, u.token)
+	if err != nil {
+		// Failure!
+		u.collectDuration(-1)
+		return startState
+	}
+
+	u.collectDuration(duration)
+
+	return startState
+}
+
+func (u *UserSimulation) listAllReviews() string {
+	_, duration, err := timeHTTPRequest("/api/list-reviews", true, "")
+	if err != nil {
+		// Failure!
+		u.collectDuration(-1)
+		return startState
+	}
+
+	u.collectDuration(duration)
+
+	return startState
+}
+
+func (u *UserSimulation) uploadReview() string {
+	log("Entering uploadPhoto state...")
+
+	// List all files in ./test-assets
+	assetsPhotos, err := os.ReadDir("./test-assets")
+	if err != nil {
+		panic(err) // Totally fine to panic here, as this is a developer error
+	}
+
+	// Pick a random photo
+	photoToUpload := assetsPhotos[rand.Intn(len(assetsPhotos))].Name()
+	photoPath := fmt.Sprintf("./test-assets/%s", photoToUpload)
+	payload := Payload{
+		title:   "Test title",
+		content: "Test content",
+		rating:  5,
+	}
+	body, duration, err := timeHTTPPostFile("/api/upload-review", payload, photoPath, photoToUpload, u.token)
+	if err != nil {
+		// Failure!
+		u.collectDuration(-1)
+		return startState
+	}
+
+	u.collectDuration(duration)
+
+	// Parse the response to get the photo ID
+	// The response is a JSON object with a key "photo_id"
+	var data map[string]interface{}
+	if err := json.Unmarshal([]byte(body), &data); err != nil {
+		log("Error: uploadPhoto JSON is not valid")
+		return startState
+	}
+
+	return startState
+}
+
+func (u *UserSimulation) Run() {
+	for {
+		nextState := ""
+		switch u.currentState {
+		case startState:
+			nextState = u.start()
+		case uploadReviewState:
+			nextState = u.uploadReview()
+		case listAllReviewsState:
+			nextState = u.listAllReviews()
+		case listPersonalReviewsState:
+			nextState = u.listPersonalReviews()
+		}
+
+		var lastRequestTime int
+		u.durationsMutex.Lock()
+		if len(u.durations) > 0 {
+			lastRequestTime = u.durations[len(u.durations)-1]
+		} else {
+			lastRequestTime = 0
+		}
+		u.durationsMutex.Unlock()
+
+		if nextState != startState && lastRequestTime > -1 {
+			// Wait 1 second before transitioning to the next state,
+			// to simulate a user thinking about what to do next,
+			// except when going back to the start state.
+			//
+			// Also, do not wait if the last request failed,
+			// since we need to stress test the system.
+			time.Sleep(1 * time.Second)
+		}
+		u.currentState = nextState
+	}
+}
+
+func (u *UserSimulation) collectHTTPRequestWithBody(url string) (string, error) {
+	body, duration, err := TimeHTTPRequestWithBody(url)
+	if err != nil {
+		u.collectDuration(-1)
+	} else {
+		u.collectDuration(duration)
+	}
+	return body, err
+}
+
+func (u *UserSimulation) collectHTTPRequest(url string) error {
+	// Do not reuse collectHTTPRequestWithBody, as we don't need the body,
+	// and we do not even want to read it from the response stream connection.
+	duration, err := TimeHTTPRequest(url)
+	if err != nil {
+		u.collectDuration(-1)
+	} else {
+		u.collectDuration(duration)
+	}
+	return err
+}
+
+func (u *UserSimulation) collectDuration(duration int) {
+	u.durationsMutex.Lock()
+	defer u.durationsMutex.Unlock()
+
+	log("Collecting duration", duration)
+
+	if duration == -1 {
+		u.failedRequests++
+	} else {
+		u.durations = append(u.durations, duration)
+	}
+}
+
+func (u *UserSimulation) ResetStatistics() UserSimulationStats {
+	u.durationsMutex.Lock()
+	defer u.durationsMutex.Unlock()
+
+	// Compute the mean
+	var sum int
+	for _, d := range u.durations {
+		sum += d
+	}
+	mean := float64(sum) / float64(len(u.durations))
+
+	// Compute the standard deviation
+	var sumSquaredDiff float64
+	for _, d := range u.durations {
+		diff := float64(d) - mean
+		sumSquaredDiff += diff * diff
+	}
+	stdDev := math.Sqrt(sumSquaredDiff / float64(len(u.durations)))
+
+	stats := UserSimulationStats{
+		ValidRequests:  len(u.durations),
+		FailedRequests: u.failedRequests,
+		TimeMean:       mean,
+		TimeStdDev:     stdDev,
+	}
+
+	// Reset the accumulated durations
+	u.durations = []int{}
+	u.failedRequests = 0
+
+	return stats
+}
